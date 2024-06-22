eef3e0b591153849956403841f73a11316d721bb
betterlucky <dwgharris@gmail.com>
2024-06-22 18:20:50 +0100

    Test

--- classics/settings.py
+++ classics/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'classics.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://cosdata_jfni_user:rNeHXFxCyz5XA7RbrIh62NtOjvWsMhYU@dpg-cpq896hu0jms738r9810-a/cosdata_jfni',
+        default='postgresql://cosdata_jfni_user:rNeHXFxCyz5XA7RbrIh62NtOjvWsMhYU@dpg-cpq896hu0jms738r9810-a.oregon-postgres.render.com/cosdata_jfni',
         conn_max_age=600
     )
 }

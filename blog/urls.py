b7ee9b2a1cfb52d63905303d69db0b04f5cc3e52
betterlucky <dwgharris@gmail.com>
2025-01-02 15:21:36 +0000

    User reg added

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
+    path('register/', views.register, name='register'),
     path("show/<int:pk>/", views.blog_detail, name="blog_detail"),
     path('film/<str:film_name>/', views.blog_film, name='blog_film'),
     path('location/<str:location_name>/', views.blog_location, name='blog_location'),
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
-from blog.forms import CommentForm
+from blog.forms import CommentForm, CustomUserCreationForm
 from blog.models import Comment, Show, Film, Location
 
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('/')  # Redirect to a success page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 def blog_index(request):
     shows = Show.objects.all().order_by("-created_on")
